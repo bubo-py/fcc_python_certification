c196c28cf93e1736a43e0f96c6ded93d78e66512
kamil <kamil.chruscinski1@gmail.com>
2020-11-09 20:14:32 +0100

    added completed last project

--- probability_calculator/prob_calculator.py
+++ probability_calculator/prob_calculator.py
@@ -11,12 +11,33 @@ class Hat:
                 self.contents.append(key)
 
     def draw(self, number):
-        chose = []
-        for x in range(number):
-            drawing = random.choice(self.contents)
-            chose.append(drawing)
-            self.contents.pop(self.contents.index(drawing))
-        return chose
+        
+        if number >= len(self.contents):
+            return self.contents
+
+        else:
+            chose = []
+            for n in range(number):
+                indexing = random.randrange(len(self.contents))
+                chose_ball = self.contents[indexing]
+                chose.append(chose_ball)
+                
+                self.contents = self.contents[0:indexing] + self.contents[indexing + 1:]
+                
+            return chose
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    pass
+    c = 0
+    for n in range(num_experiments):
+        n_hat = copy.copy(hat)
+        n_draw = n_hat.draw(num_balls_drawn)
+        bleh = True
+        for key in expected_balls.keys():
+            if n_draw.count(key) < expected_balls[key]:
+                bleh = False
+                break
+        if bleh:
+            c += 1
+
+        probability = c / num_experiments
+    return probability
